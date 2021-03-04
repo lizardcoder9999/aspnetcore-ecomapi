f6ba42b670e3c237f7f36532bbe6d2ace423a5a5
lizardcoder9999 <lizardcoder6@gmail.com>
2021-03-04 08:30:45 -0500

    Added Product interface and Product repository

--- angularApiCore/Controllers/ProductsController.cs
+++ angularApiCore/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.interfaces;
 using Core.Models;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
@@ -15,24 +16,24 @@ namespace angularApiCore.Controllers
     public class ProductsController : ControllerBase
     {
 
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repo;
 
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;
+            _repo = repo;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
+            var products = await _repo.GetProductsAsync();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
         }
 
 
