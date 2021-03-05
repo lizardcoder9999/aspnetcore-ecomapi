2b15ea73ebdb44ac6e044e9ebccb6d96478caf6e
lizardcoder9999 <lizardcoder6@gmail.com>
2021-03-05 13:19:53 -0500

    Added search functionality

--- Core/Specifications/ProductsWithFiltersCountSpecification.cs
+++ Core/Specifications/ProductsWithFiltersCountSpecification.cs
@@ -8,8 +8,10 @@ namespace Core.Specifications
     public class ProductsWithFiltersCountSpecification : BaseSpecification<Product>
     {
 
-        public ProductsWithFiltersCountSpecification(ProductSpecParams productParams) : base(x =>
-            (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId && (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId))
+        public ProductsWithFiltersCountSpecification(ProductSpecParams productParams) : base
+        
+            (x => (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
+                  (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId && (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId))
         )
         {
             
