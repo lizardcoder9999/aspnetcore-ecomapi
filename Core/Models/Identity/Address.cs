a97f8a241cb85c5081414c1ef14ac8adb848883d
lizardcoder9999 <lizardcoder6@gmail.com>
2021-03-07 11:25:03 -0500

    Added AppUser model deriving from the IdentityUser and added the address class to form a one to one relationship between users and addresses.

---
+++ Core/Models/Identity/Address.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models.Identity
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        //Convention based foreign key
+        [Required]
+        public string AppUserId { get; set; }
+
+        //One to one relationship between AppUser with Address.
+        public AppUser AppUser { get; set; }
+    }
+}
