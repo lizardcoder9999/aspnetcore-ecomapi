8c160b386834f93ddba156f2da50af02eb6fc001
lizardcoder9999 <lizardcoder6@gmail.com>
2021-03-09 11:43:43 -0500

    Added OrderProductItemUrlResolver to fix url link.

---
+++ angularApiCore/Helpers/OrderItemUrlResolver.cs
@@ -0,0 +1,28 @@
+﻿using angularApiCore.Dtos;
+using AutoMapper;
+using Core.Models.OrderAggregate;
+using Microsoft.Extensions.Configuration;
+
+namespace angularApiCore.Helpers
+{
+    public class OrderItemUrlResolver : IValueResolver<OrderItem, OrderItemDto,string>
+    {
+
+        private readonly IConfiguration _configuration;
+        
+        public OrderItemUrlResolver(IConfiguration config)
+        {
+            _configuration = config;
+        }
+        
+        public string Resolve(OrderItem source, OrderItemDto destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.ItemOrdered.PictureUrl))
+            {
+                return _configuration["ApiUrl"] + source.ItemOrdered.PictureUrl;
+            }
+
+            return null;
+        }
+    }
+}
