7fbca9c04d810b040ebeefe41ce92508466b485f
lizardcoder9999 <lizardcoder6@gmail.com>
2021-03-09 19:41:52 -0500

    Added cached attribute class

---
+++ angularApiCore/Helpers/CachedAttribute.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace angularApiCore.Helpers
+{
+    public class CachedAttribute : Attribute, IAsyncActionFilter
+    {
+        private readonly int _timeToLiveSeconds;
+
+        public CachedAttribute(int timeToLiveSeconds)
+        {
+            _timeToLiveSeconds = timeToLiveSeconds;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var cacheService = context.HttpContext.RequestServices.GetRequiredService<IResponseCacheService>();
+
+            var cacheKey = GenerateCacheKeyFromRequest(context.HttpContext.Request);
+
+            var cachedResponse = await cacheService.GetCachedResponseAsync(cacheKey);
+
+            if (!string.IsNullOrEmpty(cachedResponse))
+            {
+                //Here we will use the cached result and prevent the request from hitting the controller so it wont send a query to are database.
+                var contentResults = new ContentResult
+                {
+                    Content = cachedResponse, ContentType = "application/json", StatusCode = 200
+                };
+
+                context.Result = contentResults;
+                
+                return;
+            }
+
+            var executedContext = await next(); // move to controller
+
+            if (executedContext.Result is OkObjectResult okObjectResult)
+            {
+                await cacheService.CacheResponseAsync(cacheKey, okObjectResult.Value,
+                    TimeSpan.FromSeconds(_timeToLiveSeconds));
+            }
+        }
+
+        private string GenerateCacheKeyFromRequest(HttpRequest request)
+        {
+            var keyBuilder = new StringBuilder();
+
+            keyBuilder.Append($"{request.Path}");
+
+            foreach (var (key,value) in request.Query.OrderBy(x => x.Key))
+            {
+                keyBuilder.Append($"|{key}-{value}");
+            }
+
+            return keyBuilder.ToString();
+        }
+    }
+}
